3862250b4e9db1d20a115d940c54491b1ca3a73c
Élisson Michael <elissonmichael@gmail.com>
2021-04-11 19:41:46 -0300

    Add Notice Model

---
+++ src/main/java/com/example/demo/model/Notice.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Notice implements Serializable {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(nullable = false, updatable = false)
+  private Long id;
+  private String title;
+  private String description;
+
+  public Notice(String title, String description) {
+    this.title = title;
+    this.description = description;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription(){
+    return description;
+  }
+
+  @Override
+  public String toString() {
+    return "Notice - id: " + id + " title: " + title + " description: " + description;
+  }
+}
