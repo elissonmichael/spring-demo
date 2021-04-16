05e7570baca44d679b9680ad36376b7904981e3c
Élisson Michael <elissonmichael@gmail.com>
2021-04-16 17:42:16 -0300

    Add CrossOrigin URL

--- src/main/java/com/example/demo/NoticeResource.java
+++ src/main/java/com/example/demo/NoticeResource.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import java.util.List;
 import com.example.demo.model.Notice;
 import com.example.demo.service.NoticeService;
+import org.springframework.core.env.Environment;
 
 @RestController
 @RequestMapping("/notices")
@@ -25,6 +27,7 @@ public class NoticeResource {
     return new ResponseEntity<>(notices, HttpStatus.OK);
   }
 
+  @CrossOrigin(origins = "https://next-js-demo-sigma.vercel.app/")
   @PostMapping
   public ResponseEntity<Notice> addNotice(@RequestBody Notice notice) {
     Notice newNotice = noticeService.addNotice(notice);
